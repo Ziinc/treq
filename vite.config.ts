d28725c7cf045cb61450253c59dcd610e40912a1
TzeYiing <ty@tzeyiing.com>
2026-01-09 15:31:44 +0800

    chore: tweak vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,8 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
-
   build: {
     rollupOptions: {
-
       output: {
         manualChunks: {
           'monaco': ['monaco-editor', '@monaco-editor/react'],
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vitest/config';
-import path from 'path';
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -9,11 +8,5 @@ export default defineConfig({
     setupFiles: ['./test/setup.ts'],
     include: ['test/**/*.test.{ts,tsx}'],
     globals: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  
+  }
 });
