dbbd601db6765a7a5a3b7dde0a47686746fae055
Ziinc <ty@tzeyiing.com>
2026-03-15 16:32:14 +0800

    chore: add cloudflare pages deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -20,13 +20,7 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://treq.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Ziinc', // Usually your GitHub org/user name.
-  projectName: 'treq', // Usually your repo name.
 
   customFields: {
     featureFlags,
