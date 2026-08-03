baf6e8cb7cb435707fc01bac9669ee0f469c3934
TzeYiing <ty@tzeyiing.com>
2026-08-03 17:44:18 +0800

    chore add

--- .jjconflict-side-0/src/components/ChangesDiffViewer.tsx
+++
@@ -1,7 +0,0 @@
-export { ChangesDiffViewer } from "./changes-diff-viewer/ChangesDiffViewerMain";
-export type {
-	ChangesDiffViewerHandle,
-	ConflictComment,
-	DiffSearchData,
-} from "./changes-diff-viewer/types";
-export { ChangesDiffViewer as default } from "./changes-diff-viewer/ChangesDiffViewerMain";
