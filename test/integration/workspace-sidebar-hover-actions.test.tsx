f1d4c20c80e766e4a295b26f15e1a82d6cc3f006
TzeYiing <ty@tzeyiing.com>
2026-08-26 04:21:57 +0800

    style: sidebar hover shifting

--- test/integration/workspace-sidebar-hover-actions.test.tsx
+++ test/integration/workspace-sidebar-hover-actions.test.tsx
@@ -49,4 +49,5 @@ describe("workspace sidebar hover actions", () => {
     const actions = agentButton.parentElement as HTMLElement;
     expect(actions.className).not.toContain("absolute");
   });
+
 });
