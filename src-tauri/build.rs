3a6f5ec5a8b530f1f41e0a4515a78ba32dd17091
Ziinc <Ziinc@users.noreply.github.com>
2026-08-28 02:29:52 +0800

    fix: bundling of duckdb dynlib (#360)

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,4 +1,7 @@
 fn main() {
+  #[cfg(target_os = "macos")]
+  println!("cargo:rustc-link-arg-bin=treq=-Wl,-rpath,@executable_path/../Frameworks");
+
   if std::env::var_os("CARGO_FEATURE_TAURI_TEST").is_some() {
     napi_build::setup();
   }
